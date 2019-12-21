70293086b954cbaadb94627ce399caf3d8ba450f
Kota Hashihama <kotahashihama@gmail.com>
2019-12-22 01:19:22 +0900

    feat: Doneを投稿する機能を追加

--- app/controllers/dones_controller.rb
+++ app/controllers/dones_controller.rb
@@ -2,4 +2,23 @@ class DonesController < ApplicationController
   def index
     @dones = Done.all
   end
+
+  def new
+    @done = Done.new
+  end
+
+  def create
+    @done = Done.new(done_params)
+    @done.save
+
+    redirect_to @done
+  end
+
+  def show
+    @done = Done.find(params[:id])
+  end
+
+  def done_params
+    params.require(:done).permit(:content)
+  end
 end
