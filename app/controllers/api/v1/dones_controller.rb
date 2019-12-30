27e708b0c053362bd2a1d368a6701082564c107a
Kota Hashihama <kotahashihama@gmail.com>
2019-12-30 16:58:10 +0900

    feat: Done投稿機能を追加

--- app/controllers/api/v1/dones_controller.rb
+++ app/controllers/api/v1/dones_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::DonesController < ApplicationController
     @dones = Done.all
     render 'index', formats: :json, handlers: 'jbuilder', layout: false
   end
+
+  def create
+    @done = Done.new(content: params[:content])
+    @done.save
+    render 'show', formats: :json, handlers: 'jbuilder', layout: false
+  end
 end
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,10 @@
 <template lang="pug">
   .wrapper
+    input(v-model="newDone")
+    button(@click="createDone") 投稿
+    p プレビュー
+    | {{ newDone }}
+    p 一覧
     ul(v-for="(done, index) in dones", :key="index")
       li {{ done.content }}
 </template>
@@ -7,6 +12,7 @@
 <script>
 export default {
   data: () => ({
+    newDone: '',
     dones: []
   }),
 
@@ -18,6 +24,15 @@ export default {
     getDones () {
       this.$axios.$get('/api/v1/dones').then((res) => {
         this.dones = res
+      })
+    },
+    createDone () {
+      const params = {
+        content: this.newDone
+      }
+
+      this.$axios.$post('/api/v1/dones', params).then((res) => {
+        this.getDones()
       })
     }
   }
