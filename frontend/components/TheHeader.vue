f50bd1dc1f10baaf1a4174a6de1c3dca6e7b3efb
Kota Hashihama <kotahashihama@gmail.com>
2019-12-30 21:55:23 +0900

    feat: ヘッダーを追加

---
+++ frontend/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+.header
+  .wrapper
+    .logo Logpath
+</template>
+
+<style lang="stylus" scoped>
+.header
+  padding 8px 0
+  border-bottom solid 1px #6ebede
+  background #77c9ea
+.logo
+  color #fff
+  font-size 30px
+  font-weight bold
+</style>
