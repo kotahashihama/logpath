0b2904a10dca79b6e46c1412df39c68d9bdddbe3
Kota Hashihama <kotahashihama@gmail.com>
2019-12-31 15:33:57 +0900

    chore: ログイン認証実装の準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :dones
-  root 'static_pages#home'
-
   namespace :api do
     namespace :v1 do
       resources :users, only: [:show], param: :username
       resources :dones, only: [:index, :create, :update, :destroy]
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+    }
     end
   end
 end
